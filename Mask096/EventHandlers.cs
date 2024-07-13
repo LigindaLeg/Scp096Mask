5d050ce01fdeb3483cba768a98f540c5c73e4e51
Daniil Savchenko <95571784+LigindaLeg@users.noreply.github.com>
2024-07-14 03:50:32 +0400

    Update
    Added Translations

--- Mask096/EventHandlers.cs
+++ Mask096/EventHandlers.cs
@@ -12,10 +12,8 @@ using UnityEngine;
 
 namespace Mask096
 {
-	// Token: 0x02000004 RID: 4
 	public class EventHandlers
 	{
-		// Token: 0x0600000D RID: 13 RVA: 0x0000224C File Offset: 0x0000044C
 		public void OnUsingItemCompleted(UsingItemCompletedEventArgs ev)
 		{
 			bool flag = this.Serials.Contains(ev.Item.Serial);
@@ -31,13 +29,12 @@ namespace Mask096
 					ev.Player.EnableEffect(Exiled.API.Enums.EffectType.Ensnared, 999f, true);
 					ev.Usable.Destroy();
 					this.Serials.Remove(ev.Usable.Serial);
-					ev.Player.CustomInfo = "Надета маска";
+					ev.Player.CustomInfo = MainPlugin.Instance.Translation.Scp096Hint;
 					Timing.RunCoroutine(this.Scp096Corountine(ev.Player, player));
 				}
 			}
 		}
 
-		// Token: 0x0600000E RID: 14 RVA: 0x0000233C File Offset: 0x0000053C
 		public void OnHurt(HurtEventArgs ev)
 		{
 			bool flag = MainPlugin.Instance.Config.IsMaskOffByDamage && this.MaskEquipped.Contains(ev.Player);
@@ -48,7 +45,6 @@ namespace Mask096
 			}
 		}
 
-		// Token: 0x0600000F RID: 15 RVA: 0x00002388 File Offset: 0x00000588
 		public void AddingTarget(AddingTargetEventArgs ev)
 		{
 			bool flag = this.MaskEquipped.Contains(ev.Player);
@@ -58,7 +54,6 @@ namespace Mask096
 			}
 		}
 
-		// Token: 0x06000010 RID: 16 RVA: 0x000023B8 File Offset: 0x000005B8
 		public void OnEnragind(EnragingEventArgs ev)
 		{
 			bool flag = this.MaskEquipped.Contains(ev.Player);
@@ -67,8 +62,6 @@ namespace Mask096
 				ev.IsAllowed = false;
 			}
 		}
-
-		// Token: 0x06000011 RID: 17 RVA: 0x000023E8 File Offset: 0x000005E8
 		public void Died(DiedEventArgs ev)
 		{
 			bool flag = this.MaskEquipped.Contains(ev.Player);
@@ -78,7 +71,6 @@ namespace Mask096
 			}
 		}
 
-		// Token: 0x06000012 RID: 18 RVA: 0x00002420 File Offset: 0x00000620
 		public void Spawned(SpawnedEventArgs ev)
 		{
 			bool flag = ev.Player.Role.Type == RoleTypeId.NtfCaptain;
@@ -86,11 +78,10 @@ namespace Mask096
 			{
 				Item item = ev.Player.AddItem(ItemType.SCP268);
 				this.Serials.Add(item.Serial);
-				ev.Player.ShowHint("Вам была выдана маска для SCP-096");
+				ev.Player.ShowHint(MainPlugin.Instance.Translation.PlayerSpawnHint);
 			}
 		}
 
-		// Token: 0x06000013 RID: 19 RVA: 0x00002468 File Offset: 0x00000668
 		public IEnumerator<float> Scp096Corountine(Player cuffer, Player scp096)
 		{
 			int num;
@@ -108,8 +99,8 @@ namespace Mask096
 				bool flag2 = i > 0;
 				if (flag2)
 				{
-					cuffer.ShowHint(string.Format("Вы надеваете маску на SCP-096, осталось <color=yellow> {0} </color> секунд", i), 1f);
-					scp096.ShowHint(string.Format("На вас надевают маску, осталось <color=yellow> {0} </color> секунд", i), 1f);
+					cuffer.ShowHint(string.Format(MainPlugin.Instance.Translation.PlayerMaskHint, i), 1f);
+					scp096.ShowHint(string.Format(MainPlugin.Instance.Translation.Scp096MaskHint, i), 1f);
 					
 				}
 				yield return Timing.WaitForSeconds(1f);
@@ -118,10 +109,8 @@ namespace Mask096
 			yield break;
 		}
 
-		// Token: 0x04000007 RID: 7
 		public List<Player> MaskEquipped = new List<Player>();
 
-		// Token: 0x04000008 RID: 8
 		public List<ushort> Serials = new List<ushort>();
 	}
 }
--- Mask096/MainPlugin.cs
+++ Mask096/MainPlugin.cs
@@ -7,14 +7,12 @@ using Exiled.Events.Handlers;
 
 namespace Mask096
 {
-	// Token: 0x02000003 RID: 3
-	public class MainPlugin : Plugin<Config>
+	public class MainPlugin : Plugin<Config, Translations>
 	{
 		public override string Name => "SCP096Mask";
 		public override string Author => "Liginda & NEGRajdanin";
 		public override Version Version => new Version(1, 0, 0);
 		public override Version RequiredExiledVersion => new Version(8, 9, 6);
-		// Token: 0x0600000A RID: 10 RVA: 0x000020BC File Offset: 0x000002BC
 		public override void OnEnabled()
 		{
 			MainPlugin.Instance = this;
@@ -25,8 +23,6 @@ namespace Mask096
 			Scp096.Enraging += new CustomEventHandler<EnragingEventArgs>(this.eventHandlers.OnEnragind);
 			Scp096.AddingTarget += new CustomEventHandler<AddingTargetEventArgs>(this.eventHandlers.AddingTarget);
 		}
-
-		// Token: 0x0600000B RID: 11 RVA: 0x00002180 File Offset: 0x00000380
 		public override void OnDisabled()
 		{
 			Exiled.Events.Handlers.Player.UsingItemCompleted -= new CustomEventHandler<UsingItemCompletedEventArgs>(this.eventHandlers.OnUsingItemCompleted);
@@ -38,10 +34,8 @@ namespace Mask096
 			MainPlugin.Instance = null;
 		}
 
-		// Token: 0x04000005 RID: 5
 		public static MainPlugin Instance;
 
-		// Token: 0x04000006 RID: 6
 		public EventHandlers eventHandlers;
 	}
 }
